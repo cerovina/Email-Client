ad9eafc6a8e6d4221dd05c0b8e5bcdd0b398df55
unknown <cerovina1997@gmail.com>
2023-12-24 16:48:54 +0100

    Corrected Initialize Firebase in firebase.js

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,5 +1,5 @@
 // Import statements
-import firebase from 'firebase/app';
+import { initializeApp } from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 
@@ -15,32 +15,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 // Export auth and database
-export const auth = firebase.auth();
-export const db = firebase.database();
-
-/*
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAzqaAQ9rKf3N7IqFO4dzxzPzuAq4ZKxNI",
-  authDomain: "email-client-nikola-cerovina.firebaseapp.com",
-  projectId: "email-client-nikola-cerovina",
-  storageBucket: "email-client-nikola-cerovina.appspot.com",
-  messagingSenderId: "137188675381",
-  appId: "1:137188675381:web:0a185a9d5b48ddbfd3867c",
-  measurementId: "G-8DJRS5PTNX"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-*/
+export const auth = app.auth();
+export const db = app.database();
